f6e3138451b42a5143fecf781fc281851a7e5008
39l6 <130191016+39l6@users.noreply.github.com>
2023-05-14 00:34:23 -0600

    Create losOs.sh

---
+++ losOs.sh
@@ -0,0 +1,22 @@
+<<'COMMENT'
+Format with FAT32
+First, create the partition table by running the following command:
+Create a Fat32 partition that takes the whole space:
+Format the boot partition to FAT32:
+output mkfs.fat 4.1 (2017-01-24)
+Once done, use the command below to print the partition table and verify that everything is set up correctly:
+https://linuxize.com/post/how-to-format-usb-sd-card-linux/
+COMMENT
+
+
+sudo parted /dev/sdb --script -- mklabel msdos
+sudo parted /dev/sdb --script -- mkpart primary fat32 1MiB 100%
+sudo mkfs.vfat -F32 /dev/sdb1
+sudo parted /dev/sdb --script print
+
+<<'COMMENT'
+boot
+https://esgeeks.com/crear-usb-booteable-con-terminal-linux/
+COMMENT
+
+sudo dd bs=4M if=~/Descargas/linux.iso of=/dev/sdb conv=fdatasync
